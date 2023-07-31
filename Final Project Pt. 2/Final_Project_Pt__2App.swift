218cc4e954bf69eaf04bac3c47f5ce9f4338034f
Amaryllis Sun <sunamaryllis@gmail.com>
2023-07-31 13:25:57 -0400

    Initial Commit

---
+++ Final Project Pt. 2/Final_Project_Pt__2App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_Pt__2App.swift
+//  Final Project Pt. 2
+//
+//  Created by Scholar on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_Pt__2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
