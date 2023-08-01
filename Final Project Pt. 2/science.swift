6732e1af731cf8a371960106bf60dffb98f35f83
Amaryllis Sun <sunamaryllis@gmail.com>
2023-08-01 15:51:43 -0400

    hail mary

--- Final Project Pt. 2/science.swift
+++ Final Project Pt. 2/science.swift
@@ -8,67 +8,65 @@
 import SwiftUI
 
 struct science: View {
-    @State private var currentIndex: Int = 0
-    //A property wrapper type that updates a property while the user performs a gesture and resets the property back to its initial state when the gesture ends.
-    @GestureState private var dragOffset: CGFloat = 0
-    let newsItems = [
-        NewsItem(image: Image("science"), title: "Science", description: "Description 1"),
-        NewsItem(image: Image("math"), title: "Math", description: "Description 2")
-        
-    ]
-    
-    var body: some View {
-        
-        NavigationStack {
+    @State var currentIndex: Int = 0
+        //A property wrapper type that updates a property while the user performs a gesture and resets the property back to its initial state when the gesture ends.
+        @GestureState private var dragOffset: CGFloat = 0
+        let newsItems = [
+            NewsItem(image: Image("science"), title: "Science", description: "Description 1"),
+            NewsItem(image: Image("math"), title: "Math", description: "Description 2")
             
-            ZStack {
-                Color(red: 0.34, green: 0.51, blue: 0.34)
-                    .ignoresSafeArea()
-                VStack{
-                    Text("STEMtelligence")
-                    //.font(.title)
-                        .font(.custom("Georgia", size: 45))
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0.8117647058823529, green: 0.8117647058823529, blue: 0.9176470588235294))
-                        .padding(.top, 20.0)
+        ]
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                ZStack {
                     
-                    ForEach(0..<newsItems.count, id: \.self){index in
                         
-                            newsItems[index].image
-                                .cornerRadius(15)
-                                .opacity(currentIndex == index ? 1.0 : 0.5)
-                                .scaleEffect(currentIndex == index ? 0.7 : 0.8)
-                                .offset(x: CGFloat(index - currentIndex) * 300 + dragOffset, y: 0)
+                        ForEach(0..<newsItems.count, id: \.self){index in
+                           
+                                Text(newsItems[currentIndex].title)
+                                .font(.title)
+                                .padding(.top, -250.0)
+                            
+                                
+                                newsItems[index].image
+                                    .cornerRadius(15)
+                                    .opacity(currentIndex == index ? 1.0 : 0.5)
+                                    .scaleEffect(currentIndex == index ? 0.7 : 0.8)
+                                    .offset(x: CGFloat(index - currentIndex) * 300 + dragOffset, y: 0)
+                            
                             //                                    .resizable(resizingMode: .stretch)
                             //                                    .aspectRatio(contentMode: .fit)
                             
-                        //    Text("Science")
-                            Text(newsItems[currentIndex].title)
-                                .font(.title)
-                        
-                        NavigationLink(destination: about_us_page()) {
-                            Text("About Us")
-                                .tint(.white)
-                                .font(.custom("Georgia", size: 20))
-                                .padding()
-                                .background(Rectangle() .foregroundColor(Color(red: 0.86, green: 0.42, blue: 0.47))
-                                    .cornerRadius(15)
-                                    .shadow(radius: 15)
-            
-            
-                                )
+                            //    Text("Science")
+                            
+                            
+                            //                                            NavigationLink(destination: about_us_page()) {
+                            //                                                Text("About Us")
+                            //                                                    .tint(.white)
+                            //                                                    .font(.custom("Georgia", size: 20))
+                            //                                                    .padding()
+                            //                                                    .background(Rectangle() .foregroundColor(Color(red: 0.86, green: 0.42, blue: 0.47))
+                            //                                                        .cornerRadius(15)
+                            //                                                        .shadow(radius: 15)
+                            //
+                            //
+                            //                                                    )
+                            //                                            }
                         }
-                        } // vstack
-
-//                        .padding()
-//                        .background(Rectangle().foregroundColor(.white))
-//                        .cornerRadius(15)
-//                        .shadow(radius:15)
-//                        .padding()
-//
-                   
+                        
                     }
-              
+                    // vstack
+                    //                        .padding()
+                    //                        .background(Rectangle().foregroundColor(.white))
+                    //                        .cornerRadius(15)
+                    //                        .shadow(radius:15)
+                    //                        .padding()
+                    //
+                    
+                    
+                    
                 } // zstack
                 .gesture(
                     DragGesture()
@@ -110,14 +108,16 @@ struct science: View {
                         }
                         .padding()
                     }
-                
+                    
                     
                 }
-                            
+                
             }
-        }
-    
-}
+            
+                            }
+                        
+                    }
+
 
 struct science_Previews: PreviewProvider {
     static var previews: some View {
